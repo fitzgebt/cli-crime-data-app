b708fe5e825f4479c6b3b83c2d3e233db1c0e918
fitzgebt <fitzgebt@gmail.com>
2021-02-01 10:14:47 -0800

    built intial gather_state and gather_offense method

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,8 +1,20 @@
 class Cli
 
-    def Start
+    def start
         puts "This app gathers information on crimes committed from the FBI's Uniform Crime Report (UCR)."
-        puts "Please select a State and an offense to see more information."
+        puts "Please select a State:"
+        self.gather_state
+        puts "Please select an offense to view more information:"
+        self.gather_offense
+    end
+
+    def gather_state
+        state = gets.strip
+    end
+
+    def gather_offense(state)
+        user_state = state
+        offense = gets.strip
     end
 
 
