8778446ff28d77020792ecd1f3598d178f044424
fitzgebt <fitzgebt@gmail.com>
2021-02-01 10:07:32 -0800

    began cli class- defined Start method

--- lib/API.rb
+++ lib/API.rb
@@ -5,19 +5,18 @@ class Api
     
     @@api_key = "UtGzNmGS5GyUAcLL4RsPvczS85nKlg9RcvOASqg3"
 
-    
+    @@state = ""
+    @@offense = ""
     
     @@url = "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/motor-vehicle-theft/offense/states/wa/COUNT?API_KEY=#{@@api_key}"
 
 
 
 
-    def self.test_get
+    def self.call_api
         response = HTTParty.get(@@url)
-        binding.pry
     end
 
-    self.test_get
     
 
 
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,4 +1,9 @@
 class Cli
 
+    def Start
+        puts "This app gathers information on crimes committed from the FBI's Uniform Crime Report (UCR)."
+        puts "Please select a State and an offense to see more information."
+    end
+
 
 end
