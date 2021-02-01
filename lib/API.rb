d1a57d0600dd1cd12e3821d42f78580bb5610e5a
fitzgebt <fitzgebt@gmail.com>
2021-02-01 09:57:29 -0800

    decided which url to utilize for all future calls

--- lib/API.rb
+++ lib/API.rb
@@ -5,13 +5,14 @@ class Api
     
     @@api_key = "UtGzNmGS5GyUAcLL4RsPvczS85nKlg9RcvOASqg3"
 
-    @@url = "https://api.usa.gov/crime/fbi/sapi/api/participation/national?api_key=#{@@api_key}"
-    @@url2 = "https://api.usa.gov/crime/fbi/sapi/api/summarized/agencies/wa/offenses/2019/2020?API_KEY=@@api_key"
+    
+    
+    @@url = "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/motor-vehicle-theft/offense/states/wa/COUNT?API_KEY=#{@@api_key}"
+
 
 
 
     def self.test_get
-        url = "https://api.usa.gov/crime/fbi/sapi/api/participation/national?api_key=#{@@api_key}"
         response = HTTParty.get(@@url)
         binding.pry
     end
