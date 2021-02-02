2a001dfa2b1ad4ca4f109f9f1056ac397eb65e93
fitzgebt <fitzgebt@gmail.com>
2021-02-02 13:22:33 -0800

    Updated CLI - options/gather_year/gather_state/gather_offenses methods to incorporate looping though CLI

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -7,63 +7,82 @@ class Cli
     def start
         puts "This app gathers information on crimes committed from the FBI's Uniform Crime Report (UCR)."
         puts "Information displayed will have occurred between 2006 and 2019."
+        
         us_states_output
-        puts "Please select a State from the above list:"
-        gather_state
+         
         offense_output
-        puts "Please select an offense to view more information:"
-        gather_offense                                     # takes @state and @offense and calls API
-        puts "Data for tis offense is available from 2006 - 2019."
-        puts "For which year would you like to see data?"
-        gather_year                                        # calls CrimeData.offense_by_year(@year)
-        puts "Would you like to see another State, Offense or Year?"
-        options
+        
+        gather_year      # needs to return the CrimeData instance for specified year
+           
+        options         # this should loop through CLI instance so user can choose multiple times
         
 
 
     end
 
     def gather_state
-        binding.pry
-        @state = gets.strip
+        state = gets.strip
+        if us_states_list(state) == true
+            @state = state
+            gather_offense
+        else
+            us_states_output
+        end
     end
 
     def gather_offense
-        @offense = gets.strip
-        Api.call_api(@state, @offense)
+        offense = gets.strip
+        if offense_list(offense) == true
+            @offense = offense
+            Api.call_api(@state, @offense)
+        else
+            offense_output
     end
 
     def gather_year
-        @year = gets.strip
-        # CrimeData.offense_by_year(@year)
-    end
-
-
-    def offense_types
-        offenses_list = ["aggravated-assault", "all-other-larceny", "all-other-offenses", "animal-cruelty", "arson", "assisting-or-promoting-prostitution", "bad-checks", "betting", "bribery", "burglary-breaking-and-entering", "counterfeiting-forgery", "credit-card-automated-teller-machine-fraud", "destruction-damage-vandalism-of-property", "driving-under-the-influence", "drug-equipment-violations", "drug-violations", "drunkenness", "embezzlement", "extortion-blackmail", "false-pretenses-swindle-confidence-game", "fondling", "gambling-equipment-violation", "hacking-computer-invasion", "human-trafficking-commerical-sex-acts", "human-trafficking-commerical-involuntary-servitude", "identity-theft", "impersonation", "incest", "intimidation", "justifiable-homicide", "kidnapping-abduction", "motor-vehicle-theft", "murder-and-nonnegligent-manslaughter", "negligent-manslaughter", "operating-promoting-assiting-gambling", "curfew-loitering-vagrancy-violations", "peeping-tom", "pocket-picking", "pornography-obscence-material", "prostitution", "purchasing-prostitution", "purse-snatching", "rape", "robbery", "sexual-assult-with-an-object", "sex-offenses-non-forcible", "shoplifting", "simple-assault", "sodomy", "sports-tampering", "statutory-rape", "stolen-property-offenses", "theft-from-building", "theft-from-coin-operated-machine-or-device", "theft-from-motor-vehicle", "theft-of-motor-vehicle-parts-or-accessories", "theft-from-motor-vehicle", "weapon-law-violation", "welfare-fraud", "wire-fraud", "not-specified", "liquor-law-violations", "crime-against-person", "crime-against-property", "crime-against-society", "assault-offenses", "homicide-offenses", "human-trafficking-offenses", "sex-offenses", "sex-offenses-non-forcible", " fraud-offenses", "larceny-theft-offenses", " drugs-narcotic-offenses", "gambling-offenses", "prostitution-offenses"]
-    end
-
-    def offense_output
-        offense_types.each {|offense| puts offense}
+        puts "Data for tis offense is available from 2006 - 2019."
+        puts "For which year would you like to see data?"
+        year = gets.strip
+        if year.to_i.between?(2006,2019)
+            @year = year
+            CrimeData.offense_by_year(@year)
+        else
+            gather_year
     end
     
-    def us_states_list
+    def us_states_list(state)
         states_list = ["Alabama", "AL", "Alaska", "AK", "Arizona", "AZ", "Arkansas", "AR", "California", "CA", "Colorado", "CO", "Connecticut", "CT", "Delaware", "DE", "Florida", "FL", "Georgia", "GA", "Hawaii", "HI", "Idaho", "ID", "Illinois", "IL", "Indiana", "IN", "Iowa", "IA", "Kansas", "KS", "Kentucky", "KY", "Louisiana", "LA", "Maine", "ME", "Maryland", "MD", "Massachusetts", "MA", "Michigan", "MI", "Minnesota", "MN", "Mississippi", "MS", "Missouri", "MO", "Montana", "MT", "Nebraska", "NE", "Nevada", "NV", "New Hampshire", "NH", "New Jersey", "NJ", "New Mexico", "NM", "New York", "NY", "North Carolina", "NC", "North Dakota", "ND", "Ohio", "OH", "Oklahoma", "OK", "Oregon", "OR", "Pennsylvania", "PA", "Rhode Island", "RI", "South Carolina", "SC", "South Dakota", "SD", "Tennessee", "TN", "Texas", "TX", "Utah", "UT", "Vermont", "VT", "Virginia", "VA", "Washington", "WA", "West Virginia", "WV", "Wisconsin", "WI", "Wyoming", "WY","al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy"]
+        states_list.include?(state)
     end
 
     def us_states_output
         states_output = ["Alabama – AL", "Alaska - AK", "Arizona - AZ", "Arkansas - AR", "California - CA", "Colorado - CO", "Connecticut - CT", "Delaware - DE", "Florida - FL", "Georgia - GA", "Hawaii - HI", "Idaho - ID", "Illinois - IL", "Indiana - IN", "Iowa - IA", "Kansas - KS", "Kentucky - KY", "Louisiana - LA", "Maine - ME", "Maryland - MD", "Massachusetts - MA", "Michigan - MI", "Minnesota - MN", "Mississippi - MS", "Missouri - MO", "Montana - MT", "Nebraska - NE", "Nevada - NV", "New Hampshire - NH", "New Jersey - NJ", "New Mexico - NM", "New York - NY", "North Carolina - NC", "North Dakota - ND","Ohio - OH", "Oklahoma - OK", "Oregon - OR", "Pennsylvania - PA", "Rhode Island - RI", "South Carolina - SC", "South Dakota - SD", "Tennessee - TN", "Texas - TX", "Utah - UT", "Vermont - VT", "Virginia - VA", "Washington - WA", "West Virginia - WV", "Wisconsin - WI","Wyoming - WY"]
         states_output.each {|state| puts state}
+        puts "Please select a State from the above list:"
+        gather_state  # gets here
+    end
+
+    def offense_list(offense)
+        offenses_types = ["aggravated-assault", "all-other-larceny", "all-other-offenses", "animal-cruelty", "arson", "assisting-or-promoting-prostitution", "bad-checks", "betting", "bribery", "burglary-breaking-and-entering", "counterfeiting-forgery", "credit-card-automated-teller-machine-fraud", "destruction-damage-vandalism-of-property", "driving-under-the-influence", "drug-equipment-violations", "drug-violations", "drunkenness", "embezzlement", "extortion-blackmail", "false-pretenses-swindle-confidence-game", "fondling", "gambling-equipment-violation", "hacking-computer-invasion", "human-trafficking-commerical-sex-acts", "human-trafficking-commerical-involuntary-servitude", "identity-theft", "impersonation", "incest", "intimidation", "justifiable-homicide", "kidnapping-abduction", "motor-vehicle-theft", "murder-and-nonnegligent-manslaughter", "negligent-manslaughter", "operating-promoting-assiting-gambling", "curfew-loitering-vagrancy-violations", "peeping-tom", "pocket-picking", "pornography-obscence-material", "prostitution", "purchasing-prostitution", "purse-snatching", "rape", "robbery", "sexual-assult-with-an-object", "sex-offenses-non-forcible", "shoplifting", "simple-assault", "sodomy", "sports-tampering", "statutory-rape", "stolen-property-offenses", "theft-from-building", "theft-from-coin-operated-machine-or-device", "theft-from-motor-vehicle", "theft-of-motor-vehicle-parts-or-accessories", "theft-from-motor-vehicle", "weapon-law-violation", "welfare-fraud", "wire-fraud", "not-specified", "liquor-law-violations", "crime-against-person", "crime-against-property", "crime-against-society", "assault-offenses", "homicide-offenses", "human-trafficking-offenses", "sex-offenses", "sex-offenses-non-forcible", " fraud-offenses", "larceny-theft-offenses", " drugs-narcotic-offenses", "gambling-offenses", "prostitution-offenses"]
+        offense_types.include?(offense)
+    end
+
+    def offense_output
+        offenses_types = ["aggravated-assault", "all-other-larceny", "all-other-offenses", "animal-cruelty", "arson", "assisting-or-promoting-prostitution", "bad-checks", "betting", "bribery", "burglary-breaking-and-entering", "counterfeiting-forgery", "credit-card-automated-teller-machine-fraud", "destruction-damage-vandalism-of-property", "driving-under-the-influence", "drug-equipment-violations", "drug-violations", "drunkenness", "embezzlement", "extortion-blackmail", "false-pretenses-swindle-confidence-game", "fondling", "gambling-equipment-violation", "hacking-computer-invasion", "human-trafficking-commerical-sex-acts", "human-trafficking-commerical-involuntary-servitude", "identity-theft", "impersonation", "incest", "intimidation", "justifiable-homicide", "kidnapping-abduction", "motor-vehicle-theft", "murder-and-nonnegligent-manslaughter", "negligent-manslaughter", "operating-promoting-assiting-gambling", "curfew-loitering-vagrancy-violations", "peeping-tom", "pocket-picking", "pornography-obscence-material", "prostitution", "purchasing-prostitution", "purse-snatching", "rape", "robbery", "sexual-assult-with-an-object", "sex-offenses-non-forcible", "shoplifting", "simple-assault", "sodomy", "sports-tampering", "statutory-rape", "stolen-property-offenses", "theft-from-building", "theft-from-coin-operated-machine-or-device", "theft-from-motor-vehicle", "theft-of-motor-vehicle-parts-or-accessories", "theft-from-motor-vehicle", "weapon-law-violation", "welfare-fraud", "wire-fraud", "not-specified", "liquor-law-violations", "crime-against-person", "crime-against-property", "crime-against-society", "assault-offenses", "homicide-offenses", "human-trafficking-offenses", "sex-offenses", "sex-offenses-non-forcible", " fraud-offenses", "larceny-theft-offenses", " drugs-narcotic-offenses", "gambling-offenses", "prostitution-offenses"]
+        offense_types.each {|offense| puts offense}
+        puts "Please select an offense (exactly as shown above) to view stats on this offense:"
+        gather_offense   # gets here
     end
 
     def options
+        puts "Would you like to see another State, Offense or Year?"
         input = gets.strip
         if input == "State" || input == "state" || input == "s" || input == "STATE" || input == "S"
             gather_state
         elsif input == "Offense" || input == "offense" || input == "o" || input == "OFFENSE" || input == "O"
             gather_offense
         elsif input == "Year" || input == "year" || input == "y" || input == "YEAR" || input == "y" || input.is_integer? == true
-            if input.is_integer? == true
+            if input.is_integer? == true && input.to_i.between?(2006,2019)
                 @year = input
                 # CrimeData.offense_by_year(@year)
             else 
@@ -71,6 +90,7 @@ class Cli
             end
         else
             "Please enter 'state' or 'offense' or 'year':"
+            options
         end
         
     end
@@ -86,10 +106,7 @@ end
 
 
 
-# if input == "yes" || input == "y" || input == "Yes" || input == "YES" || input == "Y"
-#     true
-# elsif input == "no" || input == "n" || input == "No" || input == "NO" || input == "N"
-#     false
-# else
-#     "Please enter 'yes' or 'no':"
-# end
+
+# need to figure out how to loop through options/results
+# need to create CrimeData methods for calling different attrributes about each instance (self.all, all, victim age ranges, etc.)
+# screen record next coding session
--- lib/crime-data.rb
+++ lib/crime-data.rb
@@ -1,12 +1,12 @@
 class CrimeData
+    # attr_accessors defined in initialize
 
-    @@all = []
+    @@all = []          # all instances for every year of every state/offense combo sent to API
 
     def initialize(hash)
         hash.each do |key, value|
             self.class.attr_accessor(key)
             self.send("#{key}=", value)
-            
         end
         @@all << self
     end
@@ -17,3 +17,4 @@ class CrimeData
 end
 
 
+# 
